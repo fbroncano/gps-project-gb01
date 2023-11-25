df00c895caf8adc530f2a40038fc7db7ccc82a48
Fernando <fernandobroncanomorgado@gmail.com>
2023-11-25 12:49:39 +0100

    Guardado de ficheros

--- app/src/main/java/es/unex/gps/weathevent/database/WeathEventDataBase.kt
+++ app/src/main/java/es/unex/gps/weathevent/database/WeathEventDataBase.kt
@@ -10,31 +10,7 @@ import es.unex.gps.weathevent.model.User
 import es.unex.gps.weathevent.model.UserCiudadCrossRef
 
 
-@Database(version = 3, entities = [User::class, Event::class, Ciudad::class, UserCiudadCrossRef::class], exportSchema = false)
+
 abstract class WeathEventDataBase : RoomDatabase() {
-    abstract fun userDao(): UserDao
-    abstract fun eventDao() : EventDao
-    abstract fun ciudadDao() : CiudadDao
 
-    companion object {
-        private var INSTANCE: WeathEventDataBase? = null
-
-        fun getInstance(context: Context): WeathEventDataBase {
-            if (INSTANCE == null) {
-                synchronized(WeathEventDataBase::class) {
-                    INSTANCE = Room.databaseBuilder(
-                        context.applicationContext,
-                        WeathEventDataBase::class.java,
-                        "WeathEventDataBase.db" // Nombre de la base de datos
-                    ).fallbackToDestructiveMigration()
-                        .build()
-                }
-            }
-            return INSTANCE!!
-        }
-
-        fun destroyInstance() {
-            INSTANCE = null
-        }
-    }
 }
