1556e41d469c3efef1c8ffe3812cd0c50a6eaee3
Fernando <fernandobroncanomorgado@gmail.com>
2023-11-25 11:15:08 +0100

    WHEAT-21 #Implementación de servicio de respuesta de Retrofit

--- app/src/main/java/es/unex/gps/weathevent/api/ElTiempoAPI.kt
+++ app/src/main/java/es/unex/gps/weathevent/api/ElTiempoAPI.kt
@@ -5,8 +5,31 @@ import es.unex.gps.weathevent.data.api.MunicipioResponse
 import es.unex.gps.weathevent.data.api.MunicipiosResponse
 import es.unex.gps.weathevent.data.api.ProvinciaResponse
 import es.unex.gps.weathevent.data.api.ProvinciasResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+
+private const val BASE_URL = "https://www.el-tiempo.net/api/json/v2/"
+
+private val service : ElTiempoAPI by lazy {
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor())
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    retrofit.create(ElTiempoAPI::class.java)
+}
+
+fun getElTiempoService() = service
+
 interface ElTiempoAPI {
     @GET("home")
     suspend fun getHome() : HomeResponse
