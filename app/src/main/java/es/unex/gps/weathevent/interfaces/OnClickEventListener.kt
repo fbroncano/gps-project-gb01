478f2c4d51d375c08d12bed3b7a7dd6eb1183f72
Fernando <fernandobroncanomorgado@gmail.com>
2023-12-16 14:04:59 +0100

    ViewModels de Eventos gestionados

--- app/src/main/java/es/unex/gps/weathevent/interfaces/OnClickEventListener.kt
+++
@@ -1,9 +0,0 @@
-package es.unex.gps.weathevent.interfaces
-
-import es.unex.gps.weathevent.model.Event
-
-interface OnClickEventListener {
-    fun onEventClick(event: Event)
-
-    fun onEventDelete(event: Event)
-}
