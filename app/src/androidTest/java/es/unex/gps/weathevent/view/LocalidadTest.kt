917e8c1077fc152579dd40ab72f69354ec0f044f
fbroncano <fernandobroncanomorgado@gmail.com>
2023-12-22 02:22:49 +0100

    WHEAT-47 Testing de la predicción a una semana vista

--- app/src/androidTest/java/es/unex/gps/weathevent/view/LocalidadTest.kt
+++ app/src/androidTest/java/es/unex/gps/weathevent/view/LocalidadTest.kt
@@ -124,6 +124,111 @@ class LocalidadTests {
 
     }
 
+    @Test
+    fun MostrarDetallePrediccionMeteorologicaAUnaSemanaVistaDeUnaCiudad() {
+        val materialButton = onView(
+            withId(R.id.registro)
+        )
+        materialButton.perform(click())
+
+        val appCompatEditText = onView(
+            withId(R.id.nombreRegistro)
+        )
+        appCompatEditText.perform(replaceText("test registro"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            withId(R.id.usernameRegistro)
+        )
+        appCompatEditText2.perform(replaceText("testTiempoDias"), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            withId(R.id.emailRegistro)
+        )
+        appCompatEditText3.perform(replaceText("test@test.es"), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            withId(R.id.passRegistro)
+        )
+        appCompatEditText4.perform(replaceText("test123"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            withId(R.id.confirmarRegistro)
+        )
+        materialButton2.perform(click())
+
+        val frameLayout = onView(
+            withId(R.id.bottom_navigation)
+        )
+        frameLayout.check(matches(isDisplayed()))
+
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.buscarFragment), withContentDescription("Búsqueda"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottom_navigation),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        Thread.sleep(15000)
+
+        val textInputEditText = onView(
+            allOf(
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.editText),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("La Coronada"), closeSoftKeyboard())
+
+        val materialTextView = onView(
+            allOf(
+                withId(R.id.cityName), withText("La Coronada"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.search_item),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        materialTextView.perform(click())
+
+        Thread.sleep(3000)
+
+        val bottomNavigationItemView2 = onView(
+            allOf(
+                withId(R.id.proximosDiasFragment), withContentDescription("Próximos días"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.pronostico_navigation),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView2.perform(click())
+
+        val recyclerView2 = onView(
+            withId(R.id.rvProximosDias)
+        )
+        recyclerView2.check(matches(isDisplayed()))
+    }
     private fun childAtPosition(
         parentMatcher: Matcher<View>, position: Int
     ): Matcher<View> {
