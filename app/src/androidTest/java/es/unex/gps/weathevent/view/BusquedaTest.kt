a0dca5de5a0596a6ed336e4d5b04ebdfa8d9acc6
antonioo420 <ahidalgowj@alumnos.unex.es>
2023-12-22 02:36:39 +0100

    WHEAT-142 Testing

---
+++ app/src/androidTest/java/es/unex/gps/weathevent/view/BusquedaTest.kt
@@ -0,0 +1,127 @@
+package es.unex.gps.weathevent.view
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import es.unex.gps.weathevent.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class BusquedaTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(IniciarSesionActivity::class.java)
+
+    @Test
+    fun BusquedaLocalidadParaAccesoASusDatosClimatologicos() {
+        val materialButton = onView(
+            withId(R.id.registro)
+        )
+        materialButton.perform(click())
+
+        val appCompatEditText = onView(
+            withId(R.id.nombreRegistro)
+        )
+        appCompatEditText.perform(replaceText("test busqueda"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            withId(R.id.usernameRegistro)
+        )
+        appCompatEditText2.perform(replaceText("testBuscarLocalidad"), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            withId(R.id.emailRegistro)
+        )
+        appCompatEditText3.perform(replaceText("test@test.es"), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            withId(R.id.passRegistro)
+        )
+        appCompatEditText4.perform(replaceText("test"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            withId(R.id.confirmarRegistro)
+        )
+        materialButton2.perform(click())
+
+        val frameLayout = onView(
+            withId(R.id.bottom_navigation)
+        )
+        frameLayout.check(matches(isDisplayed()))
+
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.buscarFragment), withContentDescription("Búsqueda"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottom_navigation),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        Thread.sleep(15000)
+
+        // Comprueba que se ha encontrado la localidad buscada
+        val textInputEditText = onView(
+            allOf(
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.editText),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("La Coronada"), closeSoftKeyboard())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.cityName), withText("La Coronada"),
+                withParent(withParent(withId(R.id.search_item))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("La Coronada")))
+
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
