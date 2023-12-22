c9c2419d4d91cebb3b3f5135854e9bb367fb4a78
jesuchito <jcarreterol2002@gmail.com>
2023-12-22 21:04:55 +0100

    WHEAT-146 Resolución de conflictos de merge

--- app/src/test/java/es/unex/gps/weathevent/CiudadUnitTest.kt
+++ app/src/test/java/es/unex/gps/weathevent/CiudadUnitTest.kt
@@ -1,6 +1,6 @@
 package es.unex.gps.weathevent
 
-import es.unex.gps.weathevent.Model.Ciudad
+import es.unex.gps.weathevent.model.Ciudad
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
